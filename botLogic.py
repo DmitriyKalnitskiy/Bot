8205f49e3ece0cb819d291060233693730d74d2c
DmitriyKalnitskiy <161853873+DmitriyKalnitskiy@users.noreply.github.com>
2024-05-07 15:05:08 +0600

    Update botLogic.py

--- botLogic.py
+++ botLogic.py
@@ -6,26 +6,19 @@ from bot import token
 from aiogram import executor
 
 dp, bot = token()
+
 class MotivationState(StatesGroup):
     sending = State()
 
 @dp.message_handler(commands=['start'])
-async def sendWelcome(message: types.Message):
-    await message.reply("\nДля того, чтобы пользоваться ботом введите команду /motivation."
+async def sendWelcome(message: types.Message, state: FSMContext):
+    await message.reply("\nДля того, чтобы, бот отправил мотивационное изображение введите команду /photo ."
                         "\nЧтобы просмотреть возможности бота введи команду /info.")
 
-@dp.message_handler(commands=['motivation'])
-async def ask_for_motivation(message: types.Message):
-    await MotivationState.sending.set()
-    await message.reply("Отправьте команду /photo, чтобы получить мотивационное изображение.")
-
-@dp.message_handler(commands=['photo'], state=MotivationState.sending)
+@dp.message_handler(commands=['photo'])
 async def send_motivation(message: types.Message, state: FSMContext):
-
     current_directory = os.getcwd()
-
     images_directory = os.path.join(current_directory, 'image')
-
     image_files = [filename for filename in os.listdir(images_directory)
                    if filename.endswith('.jpg') or filename.endswith('.png')]
 
@@ -41,6 +34,7 @@ async def send_motivation(message: types.Message, state: FSMContext):
 
         with open(image_path, 'rb') as photo:
             await bot.send_photo(message.chat.id, photo)
+
         index += 1
         if index >= len(image_files):
             index = 0
@@ -51,6 +45,9 @@ async def send_motivation(message: types.Message, state: FSMContext):
 async def sendWelcome(message: types.Message):
     await message.reply("Данный бот создан, для того, чтобы поднять вам мотивацию, "
                         "путем отправки мотивационных картинок.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
